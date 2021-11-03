d15cfd8d74331503ec8f0834847593f4c72f0beb
Ryan May <ryan777may@gmail.com>
2021-11-03 17:34:06 -0400

    reconfigured ports

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.port
+const port = process.env.PORT
 const calls = require(`./public/src/apiCalls.js`);
 app.use(express.static(__dirname + '/public/'));
 
